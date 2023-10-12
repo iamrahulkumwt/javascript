bfeb3e4d734b76dc8face5b32771012917ae56ac
Rahul Kumawat <121931021+iamrahulkumwt@users.noreply.github.com>
2023-10-12 19:16:05 +0000

    config

---
+++ basics/01_variables.js
@@ -0,0 +1,9 @@
+const userId = 9899
+let userMail = "user@gmail.com"
+var userMob = "8739916472"
+
+// userId = 34 // we can't change const value 
+userMail = "hello@gmail.com"
+userMob = "090"
+// console.log(userMail);
+console.table([userId,userMail,userMob]);
