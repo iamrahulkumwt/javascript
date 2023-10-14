bb3acfd8c34efcb88cff6bee58fc06d001157090
Rahul Kumawat <121931021+iamrahulkumwt@users.noreply.github.com>
2023-10-14 16:48:56 +0000

    data types

---
+++ basics/02_dataType.js
@@ -0,0 +1,18 @@
+// in javascript we discuss about data types
+
+let userName = "Rahul Kumawat" // this is string data type
+let userAge = 23 // this is number data type
+let userRelation = true // this is boolean data type
+let userAccount // this is undefine data type
+let userNum = null // this is standalone data type
+
+// number = 34
+// string = " "
+// boolean = true/false
+// null = null
+// undefined = undefined
+// object = {userName:"Rahul"}
+
+// if i want to know what is data type of this variable then i use typeof
+
+console.log(typeof userRelation);
