7157fec78452b1109ca214c53b399cfeac81b87b
Rahul Kumawat <121931021+iamrahulkumwt@users.noreply.github.com>
2024-04-30 12:06:50 +0000

    Conversion of datatype

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+// let userScore =  54654
+let userScore =  undefined
+
+
+// console.log(typeof userScore);
+
+let valueIn = Number(userScore); // Converting Strings to Numbers
+// let valueIn = String(userScore); // Converting Numbers to Strings
+
+// console.log(typeof valueIn);
+console.log(valueIn);
