bc98228badcf751819cfb915bfd042a2e54bad3e
Rahul Kumawat <121931021+iamrahulkumwt@users.noreply.github.com>
2024-05-10 12:17:14 +0000

    memory type in javascript

--- basics/05_datatypeSummary.js
+++ basics/05_datatypeSummary.js
@@ -35,3 +35,35 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//+++++++++++++++++++++++++++ */ memory in javascript +++++++++++++++++++++++++++
+
+// There are two type of memory in javascript
+
+// 1. Stack (Primitive)
+// 2. Heap (Reference or Non-Primitive)
+
+
+// 1. Stack (Primitive)
+
+let myEmail = "rahul@gmail.com";
+let myAnotherEmail = myEmail;
+myAnotherEmail = "iamrahul@gmail.com";
+
+console.log(myEmail);
+console.log(myAnotherEmail);
+
+
+// 2. Heap (Reference or Non-Primitive)
+
+let userOne = {
+    userName: "rahul",
+    userLastName: "kumawat",
+}
+
+let userTwo = userOne;
+userTwo.userName = "rahulKumawat"
+
+console.log(userOne.userName);
+console.log(userTwo.userName);
