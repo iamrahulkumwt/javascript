492a9c8c555e5385460a0e384e770a4c61c9f11f
Rahul Kumawat <121931021+iamrahulkumwt@users.noreply.github.com>
2024-05-10 11:13:03 +0000

    conversion

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -8,4 +8,23 @@ let valueIn = Number(userScore); // Converting Strings to Numbers
 // let valueIn = String(userScore); // Converting Numbers to Strings
 
 // console.log(typeof valueIn);
-console.log(valueIn);
+// console.log(valueIn);
+
+
+/* ======================:: Oprations/Operator ::====================== */
+
+let str1 = "Rahul";
+let str2 = "Kumawat";
+
+let str3 = str1 + ' ' + str2;
+
+// console.log(str3);
+// console.log("1" + 2 + 3); // conversion ki pahli value string ho to sabhi ko sting ban jayegi 
+// console.log(1 + 3 + "2"); // conversion ki last value string ho to starting value ko add kar diya jayega
+
+let gameCounter = 99;
+let gameCounter2 = ++gameCounter;
+// ++gameCounter // prefix incerment => 
+// gameCounter++ // postfix incerment
+
+console.log(gameCounter);
