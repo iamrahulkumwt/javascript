96ed0c7977feffcc70a705281ff80372e7d13c63
Rahul Kumawat <121931021+iamrahulkumwt@users.noreply.github.com>
2024-07-05 12:39:38 +0000

    string

---
+++ basics/06_string.js
@@ -0,0 +1,23 @@
+const name = "Rahul";
+const repoCount = 5;
+
+// console.log(name + repoCount);
+
+console.log(`hello my name is ${name} and my repocount is ${repoCount}.`);
+
+// another way to declare a string 
+const gameName = new String('Rahul06')
+
+// console.log(gameName.length);
+// console.log(gameName.small());
+
+// const anotherGame = gameName.slice(1, 2) 
+// console.log(anotherGame);
+
+// const anotherNewGame = "    kumawat"
+// console.log(anotherNewGame);
+// console.log(anotherNewGame.trim());
+
+const url = "https://planetwebsolution%20aboutus"
+console.log(url.replace('%20', '-'));
+
