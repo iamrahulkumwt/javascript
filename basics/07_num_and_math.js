c8b55ba84e09b4cb276e3fd544f785d5f68313a6
Rahul Kumawat <121931021+iamrahulkumwt@users.noreply.github.com>
2024-07-11 10:47:18 +0000

    num and math

---
+++ basics/07_num_and_math.js
@@ -0,0 +1,26 @@
+const score = 500
+// console.log(score);
+
+const newScore = new Number(600)
+// console.log(newScore);
+
+const number = 1000000
+// console.log(number.toFixed(2));
+
+
+// ++++++++++++++ maths ++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-9));
+// console.log(Math.round(5.6));
+// console.log(Math.min(2, 4, 18, 1));
+// console.log(Math.max(2, 4, 18, 1));
+
+console.log(Math.random()); // Math.random() ki value hamesha 0 or 1 ke bich aati hai
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
