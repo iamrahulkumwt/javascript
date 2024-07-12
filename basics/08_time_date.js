e501ab3ef0f5a37301d3397c13e710ee323051cd
Rahul Kumawat <121931021+iamrahulkumwt@users.noreply.github.com>
2024-07-12 09:10:44 +0000

    time and date

---
+++ basics/08_time_date.js
@@ -0,0 +1,8 @@
+// Date
+
+let myDate = new Date()
+
+console.log(myDate.toDateString());
+console.log(myDate.getDate());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
